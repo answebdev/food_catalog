6e4aef8aad93f4a1c0c77c2209d50bdc1b03630b
ANS <36783010+answebdev@users.noreply.github.com>
2022-05-07 09:15:41 -0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+// views
+import Home from './views/Home';
+import About from './views/About';
+import Foods from './views/Foods';
+import FilteredFoods from './views/FilteredFoods';
+
+// styles
 import './App.css';
 
 // Article: https://dev.to/sanity-io/how-to-create-a-single-page-application-with-react-and-sanity-2ggl
 // https://github.com/olawanlejoel/spa-react-sanity
 // This project is a "clean project with no predefined schemas" (schemas created from scratch)
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/foods' element={<Foods />} />
+        <Route exact path='/filteredfoods' element={<FilteredFoods />} />
+        {/* <Route path='*' element={<Error />} /> */}
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
